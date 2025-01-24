7c832815fe7b09fdfe4d0911b77474b7265ee52e
prasangr <53392337+prasangr@users.noreply.github.com>
2025-01-24 21:41:54 +0530

    added readme

--- src/main/java/org/example/productservice_proxy/ProductServiceProxyApplication.java
+++ src/main/java/org/example/productservice_proxy/ProductServiceProxyApplication.java
@@ -3,6 +3,10 @@ package org.example.productservice_proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+// Proxy service is just a delegate service that calls the actual service
 @SpringBootApplication
 public class ProductServiceProxyApplication {
 
